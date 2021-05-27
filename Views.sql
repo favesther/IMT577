96d0df810b64bda29fdc78efb7d4160b6e440789
Xinyi Yang <xyang6@uw.edu>
2021-05-27 09:24:54 -0700

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,57 +1,58 @@
 CREATE View DateView AS
 SELECT   DimDateID, FullDate, DayNumberOfWeek, DayNameOfWeek, DayNumberOfMonth, DayNumberOfYear, WeekdayFlag, WeekNumberOfYear, [MonthName, MonthNumberOfYear, CalendarQuarter, CalendarYear, CalendarSemester, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy
-FROM     dbo.DimDate
+FROM     dbo.DimDate;
+
 
 -- 
 
 CREATE View ResellerView AS
 SELECT   dimLocationKey,dimSourceResellerID,dimResellerName,dimContact,dimPhoneNumber,dimEmailAddress
-FROM     dbo.dimReseller
+FROM     dbo.dimReseller;
 
 -- 
 
 CREATE View ChannelView AS
 SELECT   dimChannelID, dimSourceChannelID, dimSourceCategoryID, dimChannelCategoryName, dimChannelName
-FROM     dbo.dimChannel
+FROM     dbo.dimChannel;
 
 -- 
 
 CREATE View LocationView AS
 SELECT   dimLocationKey, dimPostalCode, dimAddress, dimCity, dimStateProvince, dimCountry
-FROM     dbo.dimLocation
+FROM     dbo.dimLocation;
 
 -- 
 
 CREATE View CustomerView AS
 SELECT   dimCustomerID, dimLocationKey, dimSourceCustomerID, dimCustomerFullName, dimCustomerFirstName, dimCustomerLastName, dimGender
-FROM     dbo.dimCustomer
+FROM     dbo.dimCustomer;
 
 -- 
 
 CREATE View StoreView AS
 SELECT   dimStoreKey, dimLocationKey, dimSourceStoreID, dimStoreName, dimStoreNumber, dimStoreManager
-FROM     dbo.dimStore
+FROM     dbo.dimStore;
 
 -- 
 
 CREATE View ProductView AS
 SELECT   dimProductID, dimSourceProductID, dimSourceProductTypeID, dimSourceProductCategoryID, dimProductName, dimProductType, dimProductCategory, dimProductRetailPrice, dimProductWholesalePrice, dimProductCost, dimProductRetailProfit, dimProductWholesaleUnitProfit, dimProductProfitMarginUnitPercent
-FROM     dbo.dimProduct
+FROM     dbo.dimProduct;
 
 -- 
 
 CREATE View ProductSalesTargetView AS
 SELECT   dimProductID, dimTargetDateKey, ProductTargetSalesAmount
-FROM     dbo.factProductSalesTarget
+FROM     dbo.factProductSalesTarget;
 
 -- 
 
 CREATE View SRCSalesTargetView AS
 SELECT   dimStoreKey, dimResellerKey, dimChannelID,dimSRCname, dimTargetDateKey, SalesTargetSalesAmount
-FROM     dbo.factSRCSalesTarget
+FROM     dbo.factSRCSalesTarget;
 
 -- 
 
 CREATE View SalesActualView AS
 SELECT   dimProductID,dimStoreKey,dimResellerKey,dimCustomerID,dimChannelID,dimSalesDateKey,dimLocationKey,dimSourceSalesHeaderID,dimSourceSalesDetailID,dimSalesAmount,dimSalesQuantity,dimSalesUnitPrice,dimSalesExtendedCost,dimSalesTotalProfit
-FROM     dbo.factSalesActual
+FROM     dbo.factSalesActual;
